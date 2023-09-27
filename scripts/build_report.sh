4ee8c1ab35ae58dd25232ac83131e66e2b4224d0
Pedroriq <pedrohrsilveira@hotmail.com>
2023-09-27 08:01:10 -0300

    devops(Report): Atualizando email

--- scripts/build_report.sh
+++ scripts/build_report.sh
@@ -1,4 +1,4 @@
 echo "Instalando pacote mailutils"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Build successful" | mail -s "Test_Report_Restaurant" ${EMAIL_LIST} --content-type="text/html"
+echo "Build successful" | mail -s "Test_Report_Restaurant" ${EMAIL_LIST}
