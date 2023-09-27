cdc973f46b8264fcb6af104cd76a1dbd5d5f3425
Pedroriq <pedrohrsilveira@hotmail.com>
2023-09-26 21:29:22 -0300

    fix(Main): Ajusta main.py
    Ajusta processo de black

--- main.py
+++ main.py
@@ -3,48 +3,51 @@ from datetime import datetime
 from Cliente import Cliente
 
 if __name__ == "__main__":
-
     counter = 0
     add_cliente = True
     lista_client = []
     while add_cliente:
-
-        init_option = int(input('1. Adicionar Cliente\n'
-                            '2. Cliente Existente\n'
-                            '3. Sair\n'))
-        print('\n-----------------------------------------------\n')
+        init_option = int(
+            input("1. Adicionar Cliente\n" "2. Cliente Existente\n" "3. Sair\n")
+        )
+        print("\n-----------------------------------------------\n")
         if init_option == 1 or init_option == 2:
-
             if init_option == 1:
                 cliente = Cliente(counter)
                 lista_client.append(cliente)
                 counter += 1
 
             elif init_option == 2:
-                mesa = int(input('Selecionar mesa: '))
-                print('\n-----------------------------------------------\n')
+                mesa = int(input("Selecionar mesa: "))
+                print("\n-----------------------------------------------\n")
                 cliente = lista_client[mesa]
 
             add_pedido = True
             while add_pedido:
-                pedido_option = int(input('1. Adicionar Pedido\n'
-                                      '2. Cancelar Pedido\n'
-                                      '3. Fechar Conta\n'
-                                      '4. Sair da area de pedidos\n'))
-                print('\n-----------------------------------------------\n')
+                pedido_option = int(
+                    input(
+                        "1. Adicionar Pedido\n"
+                        "2. Cancelar Pedido\n"
+                        "3. Fechar Conta\n"
+                        "4. Sair da area de pedidos\n"
+                    )
+                )
+                print("\n-----------------------------------------------\n")
                 if pedido_option == 1:
-                    new_pedido = input('Pedido: ')
-                    print('\n-----------------------------------------------\n')
-                    pedido_ready, tempo_de_preparo = cliente.fazer_um_pedido(new_pedido, datetime.now().strftime("%H:%M:%S"))
-                    print('\n-----------------------------------------------\n')
+                    new_pedido = input("Pedido: ")
+                    print("\n-----------------------------------------------\n")
+                    pedido_ready, tempo_de_preparo = cliente.fazer_um_pedido(
+                        new_pedido, datetime.now().strftime("%H:%M:%S")
+                    )
+                    print("\n-----------------------------------------------\n")
                 elif pedido_option == 2:
-                    canceled_pedido = input('Pedido: ')
-                    print('\n-----------------------------------------------\n')
+                    canceled_pedido = input("Pedido: ")
+                    print("\n-----------------------------------------------\n")
                     cliente.cancelar_pedido(canceled_pedido)
                 elif pedido_option == 3:
-                    gorjeta = input('Gorjeta (Y/N): ')
-                    print('\n-----------------------------------------------\n')
-                    if gorjeta == 'Y':
+                    gorjeta = input("Gorjeta (Y/N): ")
+                    print("\n-----------------------------------------------\n")
+                    if gorjeta == "Y":
                         ten4cent = True
                     else:
                         ten4cent = False
@@ -54,4 +57,3 @@ if __name__ == "__main__":
 
         else:
             break
-
