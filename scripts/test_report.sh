d317265317b8e876990186967d73a5bf9c1267fd
Pedroriq <pedrohrsilveira@hotmail.com>
2023-09-27 07:44:15 -0300

    devops(Report): Reporta por email relatorio de testes e sucesso de build

---
+++ scripts/test_report.sh
@@ -0,0 +1,10 @@
+echo "Instalando pacote mailutils"
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pytest --html=report.html
+mkdir test
+mv report.html assets test/
+zip -r test.zip test/
+cat test.zip | mail -s "Test_Report_Restaurant" ${EMAIL_LIST} -A test.zip
