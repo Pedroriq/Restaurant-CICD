bee40686930639449a1361b893b75f3a4825ee7b
Pedroriq <pedrohrsilveira@hotmail.com>
2023-09-26 21:27:16 -0300

    devops(Lint-Test-Build): Executa CI  Executa o processo de CI para lint, testes e build

--- main.py
+++ main.py
@@ -3,15 +3,55 @@ from datetime import datetime
 from Cliente import Cliente
 
 if __name__ == "__main__":
-    cliente_1 = Cliente(1)
 
-    pedido_ready_1, tempo_de_preparo_1 = cliente_1.fazer_um_pedido(
-        "sushi", datetime.now().strftime("%H:%M:%S")
-    )
-    pedido_ready_2, tempo_de_preparo_2 = cliente_1.fazer_um_pedido(
-        "carbonara", datetime.now().strftime("%H:%M:%S")
-    )
+    counter = 0
+    add_cliente = True
+    lista_client = []
+    while add_cliente:
 
-    cliente_1.cancelar_pedido(pedido_ready_1)
+        init_option = int(input('1. Adicionar Cliente\n'
+                            '2. Cliente Existente\n'
+                            '3. Sair\n'))
+        print('\n-----------------------------------------------\n')
+        if init_option == 1 or init_option == 2:
 
-    cliente_1.pagar(ten4cent=True)
+            if init_option == 1:
+                cliente = Cliente(counter)
+                lista_client.append(cliente)
+                counter += 1
+
+            elif init_option == 2:
+                mesa = int(input('Selecionar mesa: '))
+                print('\n-----------------------------------------------\n')
+                cliente = lista_client[mesa]
+
+            add_pedido = True
+            while add_pedido:
+                pedido_option = int(input('1. Adicionar Pedido\n'
+                                      '2. Cancelar Pedido\n'
+                                      '3. Fechar Conta\n'
+                                      '4. Sair da area de pedidos\n'))
+                print('\n-----------------------------------------------\n')
+                if pedido_option == 1:
+                    new_pedido = input('Pedido: ')
+                    print('\n-----------------------------------------------\n')
+                    pedido_ready, tempo_de_preparo = cliente.fazer_um_pedido(new_pedido, datetime.now().strftime("%H:%M:%S"))
+                    print('\n-----------------------------------------------\n')
+                elif pedido_option == 2:
+                    canceled_pedido = input('Pedido: ')
+                    print('\n-----------------------------------------------\n')
+                    cliente.cancelar_pedido(canceled_pedido)
+                elif pedido_option == 3:
+                    gorjeta = input('Gorjeta (Y/N): ')
+                    print('\n-----------------------------------------------\n')
+                    if gorjeta == 'Y':
+                        ten4cent = True
+                    else:
+                        ten4cent = False
+                    cliente.pagar(ten4cent)
+                else:
+                    break
+
+        else:
+            break
+
